c3a76c189de551180261ab4c8431dbdc4b6d5234
Leslie-flo-res <Leslieflores28@gmail.com>
2024-03-23 00:05:59 -0600

    quiz data and set up

---
+++ script.js
@@ -0,0 +1,14 @@
+let quizData = [
+
+    { question: "What's the capital of France?", options: ["London", "Paris", "Quebec", "Versailles"], answer: "Paris" },
+    { question: "Who painted Mona Lisa?", options: ["Michelangelo", "Albert Einstein", "Leonardo da Vinci", "Vincent van Gogh"], answer: "Leonardo da Vinci" },
+    { question: "What is the largest planet in our solar system?", options: ["Earth", "Saturn", "Neptune", "Jupiter"], answer: "Jupiter" },
+    { question: "Who wrote 'To Kill A Mockingbird'?", options: ["Lewis Carroll", "Harper Lee", "William Shakespeare", "C. S. Lewis"], answer: "" },
+    { question: "What is the chemical symbol for water?", options: ["H₂O", "W", "Ag", "H"], answer: "H₂O" },
+    { question: "Who was the first person to step on the moon?", options: ["Louis Armstrong", "Tim Armstrong", "Neil Armstrong", "Lance Armstrong"], answer: "Neil Armstrong" },
+    { question: "What country is known as the 'land of the rising sun'?", options: ["Philippines", "Japan", "Argentina", "Taiwan"], answer: "Japan" },
+    { question: "Who is known as the father of computers?", options: ["Larry Page ", "Charles Babbage", "Steve Jobs", "Bill Gates"], answer: "Charles Babbage" },
+    { question: "What is the largest ocean on earth?", options: ["Pacific", "Atlantic", "Indian", "Arctic"], answer: "Pacific" },
+    { question: "Who discovered Penicillin?", options: ["Alexander Fleming", "Edward Jenner", "Elizabeth Blackwell", "Derek Shepherd"], answer: "Alexander Fleming" },
+
+]
